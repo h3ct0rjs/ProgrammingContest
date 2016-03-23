b2981c52a73700587283ebc30868bed9ed15826e
heticor915 <hfjimenez@utp.edu.co>
2016-03-23 17:49:05 -0500

    Cubic, Beginners Solve :+1:

---
+++ URI/1143.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//feedback: hfjimenez@utp.edu.co
+int main(){
+  int n ;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    cout<<i<<" "<<i*i<<" "<<i*i*i<<endl;
+  }
+  return 0;
+
+}
