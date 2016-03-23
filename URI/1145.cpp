a8b9501c8f84d0cfa2687d07ff987608dfdcca97
heticor915 <hfjimenez@utp.edu.co>
2016-03-23 18:32:31 -0500

    Logical Sequence 2, It gives Tl :sad:

---
+++ URI/1145.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main(){
+fast;
+int x,y,numberinline=1,i=1 ;
+cin>>x>>y;
+while(i<=y){
+  while(numberinline<=x){
+    if(numberinline==x){
+      cout<<i;
+    }
+    else{
+    cout<<i<<" ";
+    i++;              //number
+    numberinline++;}
+  }
+  numberinline=1;     //flush the counter of number per line.
+  cout<<endl;
+}
+
+}
