2ec180d64ce14be289e82d265d135e021bf02300
heticor915 <hfjimenez@utp.edu.co>
2016-03-23 23:38:47 -0500

    Searching for Nessy, Beginners

---
+++ URI/1428.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main(){
+fast;
+int t,m,n;
+cin>>t;
+while(t--){
+  cin>>n>>m;
+  cout<<(n/3)*(m/3)<<endl;
+}
+return 0;
+}
