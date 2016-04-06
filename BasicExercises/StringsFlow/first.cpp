2950f3bf5fcbe3cc2b85139037dd8dcb2039d262
heticor915 <hfjimenez@utp.edu.co>
2016-04-06 14:47:47 -0500

    Testing GPG Support

---
+++ BasicExercises/StringsFlow/first.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main(){
+fast;
+char greeting[]="Hello This is Hector";
+char other[]="HECTOR";
+cout<<":::>"<<greeting<<endl;
+//cout<<strcat(other,greeting)<<endl;	//intentional ! to commit using gpg 
+cout<<"Lenght greeting:"<<strlen(greeting)<<endl;
+cout<<"Lenght other: "<<strlen(other)<<endl;
+
+return 0;
+}
