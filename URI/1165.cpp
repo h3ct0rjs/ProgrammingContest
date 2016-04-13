190860832cd9a5f942f824f21b4ad08239ff0ce2
heticor915 <hfjimenez@utp.edu.co>
2016-04-12 23:17:55 -0500

    Prime Numbers, O(sqrt(n)) implementation

--- URI/1165.cpp
+++ URI/1165.cpp
@@ -2,34 +2,27 @@
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
 
-bool isprimenumber(int n);    //prototype functions
-int x,xi;                     //global vars.
-int main(){
-fast;
-int n;
-cin>>n;               //read test cases.
-while(n--){           //meanwhile exist test cases
-  cin>>x;             //read x, and determine if it is prime.
-  if(isprimenumber(x))
-    cout<<x<<" eh primo"<<endl;
+void Isprime(int N){    //This implemetantion is O(N), our worst case is 10^7
+  int count = 0;
+  for( int i = 1;i*i<= N;++i ){ //we just need to find if is there any other divisor from 1 to sqrt(n)
+     if( N % i == 0) {
+       if( i * i == N )
+          count++;
+      else              // i < sqrt(N) and (N / i) > sqrt(N)
+          count += 2;
+        }
+  }
+  if(count == 2)
+      cout<<N<<" eh primo"<<endl;
   else
-    cout<<x<<" nao eh primo"<<endl;
+    cout<<N<<" nao eh primo"<<endl;
 }
 
-return 0;
-}
-
-bool isprimenumber(int n){
-if(x<=1)           //base case
-  return false ;
-else if(x==2)      //obviously 2 is prime
-  return true;
-  xi=sqrt(x);      //we take the square root a iterate
-  //from 3 to the result of square root of x, trying to find
-  //divisibility.
-  for(int i=3;i<=xi;i++)
-    if(x%i==0)
-       return false;    //it sends false if its no prime
-
-  return true;
+int main(){
+  fast;
+  int n,x=0;cin>>n;
+  while(n--){
+    cin>>x;
+    Isprime(x);
+  }
 }
