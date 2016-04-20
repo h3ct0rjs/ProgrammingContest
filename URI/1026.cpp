fabbabfd83b7101dd442d231e9dc551d4f750e46
heticor915 <hfjimenez@utp.edu.co>
2016-04-20 17:15:43 -0500

    Bitwise Operations 1026

---
+++ URI/1026.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+//To Carry or not to Carry, mofiz add way :)
+int main(){
+fast;unsigned int  a,b;
+while(cin>>a>>b){
+  unsigned int result=a^b;  //unsinged to expand it !!
+  cout<<result<<endl;
+}}
