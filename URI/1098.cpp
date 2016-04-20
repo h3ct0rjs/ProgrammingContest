5abffb8fff90182373c30150a21cb1dcfa0c26b2
Hector F. Jimenez <heticor915@users.noreply.github.com>
2016-04-20 08:20:26 -0500

    Update 1098.cpp :bowtie:

--- URI/1098.cpp
+++ URI/1098.cpp
@@ -1,19 +1,14 @@
 #include <bits/stdc++.h>
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-//Sequence IJ 3
-//hfjimenez...
-//the secuence is repeated 3 times, and the ith number is 2n-1,
-//jth number is the same ith + 6.
-
+//hfjimenez...IJ4 ith increments in steps of 0.2, and jth is 1+steps of 0.2  2 3
 int main(){
 fast;
 double increment=0;
 while(increment<2){
-  for(double i=1;i<=3;i++){
-    cout<<"I="<<increment<<" J="<<i+increment<<endl;    //0.2 0.2 0.2   1.2 2.2 3.2
-  }
-  increment+=0.2;
-}
+  for(double i=1;i<=3;i++)
+    cout<<"I="<<increment<<" J="<<i+increment<<endl;
 
+  increment+=0.2;
+  }
 }
