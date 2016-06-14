6de16bdaa623cd7c7fe907178955bacd85daf48c
c1b3rh4ck <hfjimenez@utp.edu.co>
2016-06-14 11:00:41 -0500

    First HackerRank Submit

---
+++ HackerRank/Tutorial/Day0.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+using namespace std;
+
+int main(){
+  string inputString; // declare a variable to hold our input
+  getline(cin, inputString); // get a line of input from cin and save it to our variable
+	cout<<"Hello, World."<<endl
+	cout<<inputString<<endl;
+	}
