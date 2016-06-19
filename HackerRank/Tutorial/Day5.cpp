950994fa4868585022ec2227711dace3c55ef009
h3ct0rjS <hfjimenez@utp.edu.co>
2016-06-19 11:34:26 -0500

    Day5

---
+++ HackerRank/Tutorial/Day5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i=1;i<=10;++i){
+        cout<<N<<" x "<<i<<" = "<<i*N<<endl;
+    }
+    /*
+    c=1;
+    while(c<=10){
+        cout<<N<<" x "<<c<<" = "<<c*N<<endl;
+        c++;
+    }
+    */ 
+}
+
